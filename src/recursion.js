406add157c11ee8e7ebad8fffdf76b920afd45b4
Morgan Galvin <morgan@ludwigthepig.com>
2019-03-22 10:27:20 +1300

    completed first eight problems

--- src/recursion.js
+++ src/recursion.js
@@ -59,11 +59,28 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n < 0) return n + 1 + sumBelow(n+1);
+  if (n === 0) return 0;
+  return n - 1 + sumBelow(n-1);
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y || x == y - 1) {
+    return [];
+  }
+  if (x > y){ 
+    if (x === y + 2){
+      return [x - 1]
+    }
+    return [x - 1].concat(range(x - 1, y));
+  }
+
+  if (x === y - 2) {
+    return [x + 1];
+  }
+  return [x + 1].concat(range(x + 1, y));
 };
 
 // 7. Compute the exponent of a number.
@@ -72,6 +89,17 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0){
+    return 1;
+  }
+  if (exp === 1){
+    return base;
+  }
+  if (exp < 0){
+    const val = 1  / base * exponent(base, exp + 1);
+    return Number(parseFloat(val).toFixed(5))
+  }
+  return base * exponent(base, exp-1);
 };
 
 // 8. Determine if a number is a power of two.
@@ -79,6 +107,13 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1){
+    return true;
+  }
+  if (n < 1){
+    return false;
+  }
+  return powerOfTwo(n/2)
 };
 
 // 9. Write a function that reverses a string.
